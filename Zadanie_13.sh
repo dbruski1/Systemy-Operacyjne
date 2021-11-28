67f68fcc2af21acbebb639dc0900681af001c61a
dbruski1 <bruski483@gmail.com>
2021-11-28 18:23:43 +0100

    Skrypty

--- Zadanie_13.sh
+++ Zadanie_13.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+rm -r /home/student/*
+
 mkdir kat1
 mkdir kat1/kat1a
-mkdir kat1/kat1b
-
-touch kat1/plik1
 touch kat1/kat1a/plik1a
-touch kat1/kat1b/plik1b
+touch kat1/kat1a/plik1b
+mkdir kat1/kat1a/kat1c
+touch kat1/plik1
+mkdir kat1/kat1b
 
 mkdir kopia
 mkdir archiwum
@@ -14,16 +16,21 @@ mkdir rozpakowane
 
 tree /home/student
 
-cd kat1/kat1b
-cp -r /home/student/kat1 home/student/kopia
+cd kat1/kat1a/kat1c
+cp -r ../../../kat1 /home/student/kopia
+
+cd /home/student
 tar -rf kat1_arch.tar /home/student/kat1
+
 mv kat1_arch.tar /home/student/archiwum
 
-cd /home/student/rozpakowane
+cd kat1/kat1a/kat1c
 tar -tvf /home/student/archiwum/kat1_arch.tar
 
 cd /home/student/kat1/kat1b
-tar -xf /home/student/archiwum/kat1_arch.tar -C home/student/rozpakowane
-rm -r -f /home/student/kopia
+tar -xf /home/student/archiwum/kat1_arch.tar -C /home/student/rozpakowane
+
+rm -r /home/student/kopia
 
 tree /home/student
+
